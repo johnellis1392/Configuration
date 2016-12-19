929493277bf6a35b6a2f0a9f4b47996f9f883b03
johnellis1392 <john.ellis1392@gmail.com>
2016-12-18 16:31:36 -0800

    Rearranging tmux conf

--- .tmux/index.sh
+++ .tmux/index.sh
@@ -1,16 +1,24 @@
+tmux_home=~/.tmux;
+custom_dir=$tmux_home/custom;
+
 # Load custom source files depending on tmux version.
 # Only tmux version 2.1 and up has certain features
 # enabled, so it is necessary to have different 
 # configurations depending on the version.
 load_tmux_version () {
-  tmux_home=~/.tmux;
   tmux_version="$(tmux -V)";
   if [[ $(echo "$tmux_version >= 2.1" | bc) -eq 1 ]]; then
-    tmux source-file $tmux_home/tmux.2-1.conf;
+    tmux source-file $custom_dir/tmux.2-1.conf;
   else 
-    tmux source-file $tmux_home/tmux.2-0.conf;
+    tmux source-file $custom_dir/tmux.2-0.conf;
   fi
   return 0;
 }
 
+# Load custom versions of tmux
 load_tmux_version;
+
+# Load special configurations
+tmux source-file $tmux_home/general.conf;
+tmux source-file $tmux_home/keys.conf;
+tmux source-file $tmux_home/interface.conf;
