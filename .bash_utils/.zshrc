c0fbb7f1fee71fca6db150560350f4a00f65228a
John Ellis <john.ellis1392@gmail.com>
2017-06-21 21:13:06 -0700

    Refactor startup shell utils

--- .bash_utils/.zshrc
+++ .bash_utils/.zshrc
@@ -1,3 +1,1 @@
-
-echo "Hello, World!"
-
+# echo "Hello, World!"
---
+++ .bash_utils/index.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Note: Always call setup first
+source $HOME/.bash_utils/setup/index.sh
+source $HOME/.bash_utils/git/index.sh
+source $HOME/.bash_utils/misc/index.sh
