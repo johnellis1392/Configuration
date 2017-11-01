821a33b1bbeea11e53237d3c1c8bc8ebe937b65e
johnellis1392 <john.ellis1392@gmail.com>
2017-11-01 11:12:53 -0700

    Update font fix documentation

--- .zsh/index.zsh
+++ .zsh/index.zsh
@@ -14,10 +14,40 @@ source $ZSH/oh-my-zsh.sh
 # iTerm2, so we need to fix these by reintroducing the
 # old font dependencies.
 #
+# Navigate to ~/.zsh/oh-my-zsh and run 'git checkout d6a36b1 themes/agnoster.zsh-theme'
+# to checkout the last-working version.
+#
 # The child ./powerline-fonts project contains the fonts
 # required by powerline / agnoster: navigate to 
 # ~/.zsh/powerline-fonts and run ./install.sh to install
 # the fonts.
+#
+# After installing powerline fonts, you need to enable them
+# by going to Terminal > Preferences > Profiles > Font.
+# Click "Change...", and Select a Powerline-enabled
+# font (suffixed by "[...] for Powerline").
+#
+# Good fonts to choose:
+# * DejaVu Sans Mono for Powerline
+# * Droid Sans Mono Dotted for Powerline
+# * Droid Sans Mono for Powerline
+# * Droid Sans Mono Slashed for Powerline
+# * Fira Mono for Powerline
+# * Inconsolata for Powerline
+# * Inconsolata-dz for Powerline
+# * Inconsolata-g for Powerline
+# * Liberation Mono for Powerline
+# * Meslo LG L DZ for Powerline
+# * Meslo LG L for Powerline
+# * Meslo LG M DZ for Powerline
+# * Meslo LG M for Powerline
+# * Meslo LG S DZ for Powerline
+# * Meslo LG S for Powerline
+# * Noto Mono for Powerline
+# * Roboto Mono for Powerline
+# * Roboto Medium for Powerline
+# * Source Code Pro for Powerline
+# * Ubuntu Mono derivative Powerline
 build_prompt() {
   RETVAL=$?
   prompt_status
