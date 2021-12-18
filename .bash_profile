bd8c20e826ff9fff503192cbd05819b28b8b5d4f
john-ellis-reify <john.ellis@reifyhealth.com>
2021-12-18 14:24:36 -0500

    Update...everyting; Vim, Nvim, Bash, Zsh, Tmux, etc configs

--- .bash_profile
+++ .bash_profile
@@ -1,2 +1,5 @@
 #!/bin/bash
 source ~/.bashrc
+. "$HOME/.cargo/env"
+export PATH=$PATH:$HOME/.local/bin
+export PATH=$PATH:$HOME/.cabal/bin
--- .zsh/misc/plugins.zsh
+++ .zsh/misc/plugins.zsh
@@ -6,14 +6,15 @@
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
+
+# aws
 plugins=(
-  aws
   brew
   docker
   docker-compose
   git
   golang
-  osx
+  macos
   tmux
 )
 
